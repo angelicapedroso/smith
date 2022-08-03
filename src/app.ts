56eb5cd3361f17d9b95c8b0f2f60619fb9093360
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-03 12:57:34 -0300

    feat: create error middleware and connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+import httpErrorMiddleware from './middleware/http.error.middleware';
 
 const app = express();
 
 app.use(express.json());
+app.use(httpErrorMiddleware);
 
 export default app;
