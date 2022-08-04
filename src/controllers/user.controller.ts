868968db5da16f2414d68719f2055a11868f3668
angelicapedroso <angelicapedroso05@gmail.com>
2022-08-04 17:00:00 -0300

    refactor: change token return

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/user.service';
 
-export const create = async (req: Request, res: Response) : Promise<Response> => {
+export const create = async (req: Request, res: Response): Promise<Response> => {
   const result = await userService.create(req.body);
-  return res.status(201).json(result);
+  return res.status(201).json({ token: result });
 };
 
 export default create;
